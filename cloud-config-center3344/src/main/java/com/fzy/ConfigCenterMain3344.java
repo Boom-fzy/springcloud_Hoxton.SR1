1475d6e192caf0d78a05a1567c1e4df5b277c31e
fzy <1109926131@qq.com>
2020-10-14 17:56:19 +0800

    config整合

---
+++ cloud-config-center3344/src/main/java/com/fzy/ConfigCenterMain3344.java
@@ -0,0 +1,16 @@
+package com.fzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
