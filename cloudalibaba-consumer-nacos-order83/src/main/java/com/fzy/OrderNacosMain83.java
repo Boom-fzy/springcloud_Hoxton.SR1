414809ad76316ee210b2a2e050e2fba6724f883d
fzy <1109926131@qq.com>
2020-10-19 17:28:01 +0800

    alibaba-nacos 服务注册中心

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/fzy/OrderNacosMain83.java
@@ -0,0 +1,24 @@
+package com.fzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(OrderNacosMain83.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/fzy/controller/OrderNacosController.java
@@ -0,0 +1,24 @@
+package com.fzy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderNacosController {
+
+	@Autowired
+	private RestTemplate restTempalte;
+
+	@Value("${service-URL}")
+	private String serviceURL;
+
+	@GetMapping(value = "/consumer/payment/nacos/{id}")
+	public String getPayment(@PathVariable("id") Integer id) {
+		return restTempalte.getForObject(serviceURL + "/payment/nacos/" + id, String.class);
+	}
+
+}
